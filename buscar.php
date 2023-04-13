1855f723117386271dc7b6ded97708bbe6766b08
lindaiely <lindaielyrodrigues@gmail.com>
2023-04-13 18:44:50 -0300

    finalizando a tela de buscar

--- buscar.php
+++ buscar.php
@@ -1,8 +1,11 @@
-<?php require_once "components/topo.php"; ?>
+<?php
+    require_once "components/topo.php";
+    require_once "conexao/db.php";
+?>
 
     <h2>BUSCAR FUNCIONÁRIO</h2>
 
-    <form action="">
+    <form action="buscar.php" method="post">
         <div class="w-50">
             <label for="nome">Nome do Funcionário:</label>
             <input type="text" name="nome" id="nome" class="field-form">
@@ -11,11 +14,40 @@
             <label for="cargo">Cargo:</label>
             <select name="cargo" id="cargo" class="field-form">
                 <option value=""></option>
+                <?php
+                    $sqlCargo = "SELECT idcargo, nomecargo FROM cargos ORDER BY nomecargo";
+                    $resultSetCargo = $conn->query($sqlCargo); 
+                    if(mysqli_num_rows($resultSetCargo) > 0){
+                        while($registroDbCargo = mysqli_fetch_assoc($resultSetCargo)){
+                            echo "<option value=" . $registroDbCargo["idcargo"] . ">" . $registroDbCargo["nomecargo"] . "</option>";
+                        }
+                    }
+                ?>
             </select>
         </div>
         <div class="clear"></div>
         <input type="submit" value="Buscar" class="btn">
     </form>
+
+    <?php
+    if($_POST){
+        $nome = $_POST["nome"];
+        $cargo = $_POST["cargo"];
+
+        $sql = "SELECT f.idfuncionario, f.nome, f.salario, c.nomecargo, e.estado
+        FROM funcionarios f
+        INNER JOIN cargos c ON f.cargos_idcargo = c.idcargo
+        INNER JOIN enderecos e ON e.funcionarios_idfuncionario = f.idfuncionario
+        WHERE f.nome LIKE '$nome%'";
+
+        if($cargo != ""){
+            $sql .= " AND c.idcargo = " . $cargo;
+        }
+
+        $resultSet = $conn->query($sql);
+        if(mysqli_num_rows($resultSet) > 0){
+
+        ?>
 
     <table class="my-table">
         <thead>
@@ -28,14 +60,19 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th> - </th>
-                <th> - </th>
-                <th> - </th>
-                <th> - </th>
-                <th> - </th>
-            </tr>
+            <?php while($registroDb = mysqli_fetch_assoc($resultSet)){ ?>
+                <tr>
+                    <td><?=$registroDb["idfuncionario"]?></td>
+                    <td><?=$registroDb["nome"]?></td>
+                    <td>R$ <?=number_format($registroDb["salario"], 2, ",", ".")?></td>
+                    <td><?=$registroDb["nomecargo"]?></td>
+                    <td><?=$registroDb["estado"]?></td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
+    <?php
+        }
+    } ?>
 
 <?php require_once "components/rodape.php"; ?>
